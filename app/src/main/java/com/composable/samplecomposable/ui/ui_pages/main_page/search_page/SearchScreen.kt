7427d31930444ce7a51df9ea9ae593a520d173d4
Gopikrishnan N <gopikrishnan@constient.com>
2022-10-03 23:28:59 +0530

    search pages with grid

--- app/src/main/java/com/composable/samplecomposable/ui/ui_pages/main_page/search_page/SearchScreen.kt
+++ app/src/main/java/com/composable/samplecomposable/ui/ui_pages/main_page/search_page/SearchScreen.kt
@@ -1,8 +1,14 @@
 package com.composable.samplecomposable.ui.ui_pages.main_page.search_page
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
@@ -16,12 +22,25 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+import com.composable.samplecomposable.R
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -45,7 +64,169 @@ fun SearchSurfaceView() = Surface(modifier = Modifier.fillMaxSize()) {
 
 @Composable
 fun SearchBody() {
-    SearchBar()
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp
+    val screenWidth = configuration.screenWidthDp
+
+    LazyColumn {
+        item {
+            Column {
+                Spacer(modifier = Modifier.height(8.dp))
+                SearchBar()
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
+        item {
+            LazyVerticalGrid(
+                modifier = Modifier
+                    .height((screenHeight * .85).dp)
+                    .padding(horizontal = 12.dp),
+                columns = GridCells.Fixed(2)
+            ) {
+                items(10) { count ->
+                    BoxWithConstraints {
+                        Box(modifier = Modifier.padding(5.dp)) {
+                            Card(
+                                modifier = Modifier.width((screenWidth * .45).dp),
+                                backgroundColor = colorResource(id = R.color.white),
+                                elevation = 6.dp,
+                            ) {
+                                Column {
+                                    Spacer(modifier = Modifier.height(8.dp))
+                                    SubcomposeAsyncImage(
+                                        model = "https://www.freepnglogos.com/uploads/burger-png/burger-png-png-images-yellow-images-12.png",
+                                        loading = {
+//                                        if (painter.state is AsyncImagePainter.State.Loading || painter.state is AsyncImagePainter.State.Error) {
+//                                            CircularProgressIndicator(color = colorResource(id = R.color.black))
+//                                        } else {
+                                            SubcomposeAsyncImageContent(
+                                                painter = painterResource(
+                                                    id = R.drawable.tuk_in_logo
+                                                )
+                                            )
+//                                        }
+                                        },
+                                        contentDescription = "$count",
+                                        modifier = Modifier
+                                            .size(120.dp)
+                                            .align(alignment = Alignment.CenterHorizontally),
+                                    )
+                                    Text(
+                                        text = "Bacon Burger",
+                                        style = TextStyle(
+                                            fontWeight = FontWeight.Black,
+                                            color = colorResource(id = R.color.black),
+                                            fontSize = 14.sp,
+                                        ),
+                                        modifier = Modifier.padding(horizontal = 10.dp),
+                                    )
+                                    Text(
+                                        text = "yahoo camida",
+                                        style = TextStyle(
+                                            fontWeight = FontWeight.Normal,
+                                            color = colorResource(id = R.color.black).copy(.5F),
+                                            fontSize = 12.sp,
+                                        ),
+                                        modifier = Modifier.padding(horizontal = 10.dp),
+                                    )
+                                    Spacer(modifier = Modifier.height(6.dp))
+                                    val offersAmountText =
+                                        buildAnnotatedString {
+                                            withStyle(
+                                                style = SpanStyle(
+                                                    Color.Black, fontSize = 14.sp
+                                                )
+                                            ) {
+                                                append("   ₹ 150")
+                                            }
+                                            withStyle(
+                                                style = SpanStyle(
+                                                    Color.Red,
+                                                    fontSize = 13.sp,
+                                                    fontWeight = FontWeight.SemiBold,
+                                                )
+                                            ) {
+                                                append(" 40% off")
+                                            }
+                                        }
+                                    Spacer(modifier = Modifier.height(5.dp))
+                                    Row {
+                                        Text(text = offersAmountText)
+                                        Spacer(modifier = Modifier.width(5.dp))
+                                        Box(
+                                            modifier = Modifier.border(
+                                                width = .5.dp,
+                                                shape = RoundedCornerShape(2.dp),
+                                                brush = Brush.horizontalGradient(
+                                                    listOf(
+                                                        Color.Black.copy(.5f),
+                                                        Color.Black.copy(.5f)
+                                                    )
+                                                )
+                                            )
+                                        ) {
+                                            Text(
+                                                text = "ADD",
+                                                style = TextStyle(
+                                                    color = Color(0xff3b732e),
+                                                    fontSize = 10.sp,
+                                                    fontWeight = FontWeight.Medium,
+                                                ),
+                                                modifier = Modifier.padding(
+                                                    horizontal = 10.dp, vertical = 2.dp
+                                                ),
+                                            )
+                                        }
+                                    }
+                                    Spacer(modifier = Modifier.height(12.dp))
+                                }
+                            }
+
+                        }
+                        BoxWithConstraints {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_tag_icon),
+                                contentDescription = "ic_tag_icon",
+                                modifier = Modifier
+                                    .width(width = 90.dp)
+                                    .height(height = 25.dp),
+                                contentScale = ContentScale.FillBounds,
+                                alignment = Alignment.Center
+                            )
+                            val bestSellerText = buildAnnotatedString {
+                                withStyle(
+                                    style = SpanStyle(
+                                        Color.White,
+                                        fontSize = 10.sp,
+                                        fontWeight = FontWeight.SemiBold,
+                                    ),
+                                ) {
+                                    append("BEST")
+                                }
+                                withStyle(
+                                    style = SpanStyle(
+                                        Color.White,
+                                        fontSize = 10.sp,
+                                        fontWeight = FontWeight.SemiBold,
+                                    )
+                                ) {
+                                    append(" SELLER")
+                                }
+                            }
+
+                            Text(
+                                text = bestSellerText, modifier = Modifier.padding(
+                                    start = 8.dp, top = 5.dp
+                                ), style = TextStyle(
+                                    color = Color.White, fontSize = 10.sp
+                                )
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Composable
@@ -77,7 +258,7 @@ fun SearchBar() {
             .padding(horizontal = 4.dp)
             .height(40.dp),
         fontSize = 12.sp,
-        placeholderText = "Search product..."
+        placeholderText = "What are you looking for?"
     )
 }
 
